19739e26dc939f01e0b068da4017f07d2c6a85a4
Koray Yenal <60506157+korayyenal@users.noreply.github.com>
2020-03-10 21:21:38 +0300

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,28 @@
+import csv
+from bs4 import BeautifulSoup
+import urllib 
+import pandas as pd
+import wbdata
+from hw2function import linearregression
+
+def readdata():
+
+  countries = [i['id'] for i in wbdata.get_country(incomelevel="HIC", display=False)]
+
+  indicator1 = {"NY.GDP.PCAP.PP.KD": "gdppc", "IP.JRN.ARTC.SC": "Scientific_and_technical_journal_articles"}
+  indicator2 = {"NY.GDP.PCAP.PP.KD": "gdppc", "SE.ADT.1524.LT.FM.ZS": "Literacyrate"}
+
+  df = wbdata.get_dataframe(indicator1, country=countries, convert_date=True)
+  df2=  wbdata.get_dataframe(indicator2, country=countries, convert_date=True)
+
+  df = df.dropna()
+  df2 = df2.dropna()
+
+  df["uniqid"]=(df.country +df.date)
+  df2["uniqid"]=(df2.country +df2.date)
+
+  dfmerge = df.merge(df2,on="uniqid")
+  dfmerge = dfmerge.drop(['country_y','date_y','gdppc_y'], axis=1)
+  dfmerge = dfmerge.rename(columns = {"uniqid":"Unique_ID","date_x": "Date","country_x": "Country","gdppc_x": "GDPpc","Scientific_and_technical_journal_articles": "Scientific_And_Technical_Journal_Articles","Literacyrate": "Literacy_Rate"})
+
+  dfmerge.to_csv("dfmerge.csv")
