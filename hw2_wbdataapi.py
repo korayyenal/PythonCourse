23c560ecad0722d881639f480750aeb2378b370c
Koray Yenal <60506157+korayyenal@users.noreply.github.com>
2020-03-13 12:44:56 +0300

    Update hw2_wbdataapi.py

--- hw2_wbdataapi.py
+++ hw2_wbdataapi.py
@@ -13,12 +13,18 @@ def readdata():
   x2 = wbdata.get_data("EG.ELC.ACCS.RU.ZS", data_date = data_date, country = countries, pandas = True)
   y = wbdata.get_data("NY.GDP.PCAP.PP.KD", data_date = data_date, country = countries, pandas = True)
   data = pd.concat([x1, x2, y], axis = 1)
-
+  data = data.dropna(axis=0, how='all')
   
+  X = pd.concat([x1,x2], axis = 1)
+  X = X.dropna(axis=0, how='all')
+
   data.columns = ["Scientific and technical journal articles", "Access to electricity, rural","GDPpc"]
+  X.columns = ["Scientific and technical journal articles", "Access to electricity, rural","GDPpc"]
 
   x1 = data["Scientific and technical journal articles"].tolist()
   x2 = data["Access to electricity, rural"].tolist()
   y = data["GDPpc"].tolist()
   
-  return (x1,x2,y)
+  data.to_csv('data.csv')
+  
+  return (X,x1,x2,y)
